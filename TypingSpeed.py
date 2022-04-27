f597c2305f00abf18902658ec983845d67fe7f5d
SaileshBK <sailesh.bk1@icloud.com>
2022-04-26 23:03:03 -0500

    changing background image

--- TypingSpeed.py
+++ TypingSpeed.py
@@ -5,3 +5,29 @@ import sys
 import time
 import random
 
+class TypingSpeed:
+
+    def __init__(self):
+        self.w=1920
+        self.h=1080
+
+
+        pygame.init()
+
+        self.bg = pygame.image.load('screen1.png')
+        self.bg = pygame.transform.scale(self.bg, (500,750))
+
+
+        self.screen = pygame.display.set_mode((self.w,self.h))
+        pygame.display.set_caption('Type Speed test')
+
+
+
+
+    def run(self):
+        pygame.display.update()
+
+
+
+
+TypingSpeed().run()
