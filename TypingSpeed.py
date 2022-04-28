19de1b5b58b2083e99d0187095400c8a991c06bc
SaileshBK <sailesh.bk1@icloud.com>
2022-04-27 21:51:30 -0500

    background changed

--- TypingSpeed.py
+++ TypingSpeed.py
@@ -6,18 +6,27 @@ import time
 import random
 
 
-
-WIDTH, HEIGHT = 900, 500
+#setting windows width and height
+WIDTH, HEIGHT = 1000, 550
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Typing Speed")
+#loading image and scaling it to width and height
+background_image = pygame.image.load("screen1.png").convert()
+background_image = pygame.transform.scale(background_image,(WIDTH,HEIGHT))
+
 
 def main():
 
     run = True
 
     while run:
+        WIN.blit(background_image,[0,0])
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        pygame.display.update()
     pygame.quit()
 
 
