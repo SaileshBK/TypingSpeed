171c23f4e402b732d14e82b01df831933046d17b
SaileshBK <sailesh.bk1@icloud.com>
2022-04-28 22:42:44 -0500

    some changes

--- TypingSpeed.py
+++ TypingSpeed.py
@@ -6,31 +6,46 @@ import time
 import random
 
 from pyparsing import White
-pygame.init()
 
 #setting windows width and height
 WIDTH, HEIGHT = 1000, 550
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Typing Speed")
 #loading image and scaling it to width and height
-background_image = pygame.image.load("screen1.png").convert()
-background_image = pygame.transform.scale(background_image,(WIDTH,HEIGHT))
+
 TEXT_C = (240,240,240)
+pygame.init()
 
 
+
+def get_sentence():
+    with open("sentences.txt", "r") as file:
+        text = file.read()
+        text.strip('\n')
+        return text
+    # f = open('sentences.txt').read()
+    # sentences = f.split('\n')
+    # sentence = random.choice(sentences)
+    # return sentence
 
 def main():
-
-    run = True
     word = get_sentence()
+    #tmp_text = draw_text(word, 28, TEXT_C )
+
+    font = pygame.font.SysFont(word, 60)
+    text = font.render(word, True,TEXT_C)
+    text_rect = text.get_rect(topleft=(50,50))
+
+    background_image = pygame.image.load("screen1.png")
+    background_image = pygame.transform.scale(background_image,(WIDTH,HEIGHT))
+
+    #blit function takes the image variable and coordinates of the image
+    WIN.blit(background_image,[0,0])
+    WIN.blit(text, text_rect)
+    
+    run = True
 
     while run:
-
-        #blit function takes the image variable and coordinates of the image
-        WIN.blit(background_image,[0,0])
-        draw_text(WIN, word, 200, 28, TEXT_C )
-
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -41,18 +56,9 @@ def main():
         pygame.display.update()
     pygame.quit()
 
-def draw_text(WIN, msg, y ,fsize, color):
-    font = pygame.font.Font(None, fsize)
-    text = font.render(msg, 1,color)
-    text_rect = text.get_rect(topleft=(50,50))
-    WIN.blit(text, text_rect)
-    #pygame.display.update()
 
-def get_sentence():
-    f = open('sentences.txt').read()
-    sentences = f.split('\n')
-    sentence = random.choice(sentences)
-    return sentence
+
+
 
 if __name__ == "__main__":
     main()
