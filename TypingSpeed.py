690eb3267438244f91cf0b3be381d5ac3a6a5077
SaileshBK <sailesh.bk1@icloud.com>
2022-04-29 21:25:36 -0500

    pg

--- TypingSpeed.py
+++ TypingSpeed.py
@@ -1,5 +1,5 @@
 #importing libraries
-import pygame
+import pygame as pg
 from pygame.locals import *
 import sys
 import time
@@ -9,12 +9,12 @@ from pyparsing import White
 
 #setting windows width and height
 WIDTH, HEIGHT = 1000, 550
-WIN = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("Typing Speed")
+WIN = pg.display.set_mode((WIDTH,HEIGHT))
+pg.display.set_caption("Typing Speed")
 #loading image and scaling it to width and height
 
 TEXT_C = (240,240,240)
-pygame.init()
+pg.init()
 
 
 
@@ -29,31 +29,41 @@ def get_sentence():
 def main():
     word = get_sentence()
     #initializing font
-    font = pygame.font.SysFont(word, 60)
+    font = pg.font.SysFont(word, 60)
     #rendering font 
     text = font.render(word, True,TEXT_C)
     #posotioning the text from text file.
     text_rect = text.get_rect(topleft=(50,50))
     #load image
-    background_image = pygame.image.load("screen1.png")
-    background_image = pygame.transform.scale(background_image,(WIDTH,HEIGHT))
+    background_image = pg.image.load("screen1.png")
+    background_image = pg.transform.scale(background_image,(WIDTH,HEIGHT))
 
     #blit function takes the image variable and coordinates of the image.
     WIN.blit(background_image,[0,0])
     WIN.blit(text, text_rect)
+    #pg.draw.rect(WIN,(255,192,25), (50,250,650,50), 2)
+
+
+    pg.draw.rect(WIN,(255,213,102), (50,250,650,50), 2)
+    # update the text of user input
+    # self.draw_text(self.screen, self.input_text, 274, 26,(250,250,250))
+    # pg.display.update()
+    user_input = ''
+    
+
     
     run = True
 
     while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
                 run = False
         
         
         #updating display
     
-        pygame.display.update()
-    pygame.quit()
+        pg.display.update()
+    pg.quit()
 
 
 
