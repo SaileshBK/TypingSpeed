a93e3bf27a81483772fa608fd141a0a96867a4ac
SaileshBK <sailesh.bk1@icloud.com>
2022-05-03 22:04:46 -0500

    class

--- TypingSpeed.py
+++ TypingSpeed.py
@@ -1,10 +1,13 @@
 #importing libraries
+from ast import In
 import pygame as pg
 from pygame.locals import *
 
 import sys
 import time
 import random
+
+from InputBox import *
 
 from pyparsing import White
 
@@ -15,6 +18,8 @@ pg.display.set_caption("Typing Speed")
 #loading image and scaling it to width and height
 
 TEXT_C = (240,240,240)
+COLOR_INACTIVE = pg.Color('lightskyblue3')
+COLOR_ACTIVE = pg.Color('dodgerblue2')
 pg.init()
 
 
@@ -45,41 +50,68 @@ def main():
     #pg.draw.rect(WIN,(255,192,25), (50,250,650,50), 2)
 
 
-    input_box = pg.draw.rect(WIN,(255,213,102), (50,250,650,50), 2)
+    input_box1 = InputBox(100, 100, 140, 32)
+    input_boxes = [input_box1]
+    done = False
+
+    while not done:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
+            for box in input_boxes:
+                box.handle_event(event)
+
+        for box in input_boxes:
+            box.update()
+
+        #screen.fill((30, 30, 30))
+        for box in input_boxes:
+            box.draw(screen)
+
+        pg.display.update()
+
+
+
+
+
+
+
+    #input_box = pg.draw.rect(WIN,(255,213,102), (50,250,650,50), 2)
     # update the text of user input
     # self.draw_text(self.screen, self.input_text, 274, 26,(250,250,250))
     # pg.display.update()
-    user_input = ''
     
 
     
-    run = True
-    active = False
+    # run = True
+    # active = False
 
-    while run:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                run = False
+    # while run:
+    #     for event in pg.event.get():
+    #         if event.type == pg.QUIT:
+    #             run = False
 
-            if event.type == pg.MOUSEBUTTONUP:
-                # in case user click the input box.
-                if input_box.collidepoint(event.pos):
-                    active = not active
-                else:
-                    active = False
-            if event.type == pg.KEYDOWN:
-                if active:
-                    if event.key ==pg.K_RETURN:
-                        print(user_input)
-                        user_input = ''
-                    elif event.key == pg.K_BACKSPACE:
-                        user_input = user_input[:-1]
-                    else:
-                        user_input += event.unicode
+    #         if event.type == pg.MOUSEBUTTONDOWN:
+    #             # in case user click the input box.
+    #             if input_box.collidepoint(event.pos):
+    #                 active = not active
+    #             else:
+    #                 active = False
+
+    #             color = COLOR_ACTIVE if active else COLOR_INACTIVE
+    #         if event.type == pg.KEYDOWN:
+    #             if active:
+    #                 if event.key ==pg.K_RETURN:
+    #                     print(user_input)
+    #                     user_input = ''
+    #                 elif event.key == pg.K_BACKSPACE:
+    #                     user_input = user_input[:-1]
+    #                 else:
+    #                     user_input += event.unicode
 
         
         
-        #updating display
+    #     #updating display
     
         pg.display.update()
     pg.quit()
